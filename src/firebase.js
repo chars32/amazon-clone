086727b3ef175e8c646ac77ee0b0c49c6c5ea366
Carlos Garcia Castellanos <chars24@gmail.com>
2020-09-08 23:38:14 -0500

    1. Setup project and Header component

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDe8yAo7b_JRr6p0yl9DdxyW5G6ttJLZkc",
+  authDomain: "clone-96ac2.firebaseapp.com",
+  databaseURL: "https://clone-96ac2.firebaseio.com",
+  projectId: "clone-96ac2",
+  storageBucket: "clone-96ac2.appspot.com",
+  messagingSenderId: "394313627294",
+  appId: "1:394313627294:web:21d0ceb1e61bf5363ced8f",
+  measurementId: "G-HHLDEQJ4EL",
+};
+
+export default firebaseConfig;
