44d9543926349814fe0060a95a3f2ee461760433
Carlos Garcia Castellanos <chars24@gmail.com>
2020-09-09 22:09:47 -0500

    2.Home done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 
 import Header from "./Components/Header.jsx";
+import Home from "./Components/Home.jsx";
 
 function App() {
   return (
     // BEM
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
